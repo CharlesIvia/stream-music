40a2b2f59454047e8cd09f177fea64108c334049
Charles Ivia <iviacharles@gmail.com>
2020-06-03 16:10:33 +0300

    Added logic to load song details

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ const progressContainer = document.getElementById("progress-container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
 
+//Set somg title
+
+const songs = ["hey", "summer", "ukulele"];
+
+//Keep track of song
+
+let songIndex = 2;
+
+//Load song details into DOM initialy
+
+loadSong(songs[songIndex]);
+
+//Update song details
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
