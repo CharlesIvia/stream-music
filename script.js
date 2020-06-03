4a7ecd7168f60cc88b341d72ed625807c510fe67
Charles Ivia <iviacharles@gmail.com>
2020-06-03 16:39:17 +0300

    Created logic to update progress bar@

--- script.js
+++ script.js
@@ -77,6 +77,14 @@ function nextSong() {
   playSong();
 }
 
+//Update progress bar
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
 //Set progress bar
 
 function setProgress(e) {
